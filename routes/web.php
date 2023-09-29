f26515767594a7d4d9a9a4e95999381c2397191f
alessandro-rivas-dev <alessandro.rivas@goes.gob.sv>
2023-09-29 09:58:33 -0600

    :sparkles: Updating: README.md and index route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('started');
 });
 
 Route::get('/started', function () {
-    return view('started');
+    return view('welcome');
 });
